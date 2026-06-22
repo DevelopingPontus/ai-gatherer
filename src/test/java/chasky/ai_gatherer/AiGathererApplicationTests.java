83dd93417fb695632eae26971e825d9851426d7d
DevelopingPontus <ipont35@gmail.com>
2026-06-22 09:26:00 +0200

    first commit

---
+++ src/test/java/chasky/ai_gatherer/AiGathererApplicationTests.java
@@ -0,0 +1,13 @@
+package chasky.ai_gatherer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGathererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
